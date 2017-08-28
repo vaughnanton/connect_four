fb3e4ce110b633f9488385a80710b8e5c5e564c2
Vaughn Antonyan <vaughnanton@gmail.com>
2017-08-28 16:37:55 -0700

    Unicode test

--- connect_four.rb
+++ connect_four.rb
@@ -42,33 +42,68 @@ class Game
   attr_accessor :game_over
 
   def board
-    horrizontals = "---+---+---+---+---+---+---"
+    board = [
+      %w(_ _ _ _ _ _ _),
+      %w(_ _ _ _ _ _ _),
+      %w(_ _ _ _ _ _ _),
+      %w(_ _ _ _ _ _ _),
+      %w(_ _ _ _ _ _ _),
+      %w(_ _ _ _ _ _ _),
+      %w(1 2 3 4 5 6 7)]
+    puts board
   end
 
-  def move(player)
-  end
-
-  def win_check(player)
-  end
-
-  def draw_check
-  end
-
-  def end_win(player)
-  end
-
-  def end_draw
-  end
-
-  def set_player
-  end
-
-  def play_again
+  def get_answer
+    gets.chomp
   end
 
   def begin_game
+    puts "Welcome to Connect-Four! The goal of the game is to connect 4 pieces in any orientation on the 6x7 playing grid. Player 1 - \u+25E6, please input your name:"
+    @player1 = get_answer
+    puts "Player 2, \u+25CF, please input your name:"
+    @player2 = get_answer
+    board FINISH
+    loop_game
   end
 
+  def end_game(player)
+    puts "#{player} won the game! Play again? (Y/N)"
+    answer = get_answer.upcase
+    answer == "Y" ? restart_game : exit
+  end
+
+  def restart_game
+    board = [
+     %w(_ _ _ _ _ _ _),
+     %w(_ _ _ _ _ _ _),
+     %w(_ _ _ _ _ _ _),
+     %w(_ _ _ _ _ _ _),
+     %w(_ _ _ _ _ _ _),
+     %w(_ _ _ _ _ _ _),
+     %w(1 2 3 4 5 6 7)]
+   @turns = 1
+   puts "Game restarted!"; board
+   loop_game
+ end
+
+  def loop_game
+    until winning_condition?
+      player = @turns.odd ? @player1 : @player2
+      puts "#{player}, in which column do you want to drop your piece?"
+      answer = get_answer.to_i
+      valid_move?(answer, player) ? make_move(answer, player) : (p 'Error, try again': loop_game)
+    end
+  end_game(player)
+  end
+
+  #------------------- making moves --------------------------
+
+  def valid_move(answer, _player)
+    (answer.between?(1,7) && @board[0][answer-1] == '_') ? true : false
+  end
+
+  def make_move(column, player)
+    color = player == @player1 ? ""
 end
 
 game = Game.new
